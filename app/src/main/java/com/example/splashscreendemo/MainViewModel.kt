94af1617882b4acaf5071d168395195da89c4d66
ktenzweiler <ktenzweiler@gmail.com>
2023-04-19 07:50:33 -0500

    initial commit

---
+++ app/src/main/java/com/example/splashscreendemo/MainViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.splashscreendemo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            // simulate a network call
+            delay(3000L)
+            _isLoading.value = false
+        }
+    }
+}
